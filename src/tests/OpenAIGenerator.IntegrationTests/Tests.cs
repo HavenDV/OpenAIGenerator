f14bb68696bdb4543dc5791f34d67740d62c1d9c
Konstantin S <havendv@gmail.com>
2023-04-08 13:13:09 +0400

    feat: Fixed problems. Added IntegrationTests.

---
+++ src/tests/OpenAIGenerator.IntegrationTests/Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace H.Ipc.Generator.IntegrationTests;
+
+[TestClass]
+public class NSwagGeneratorTests
+{
+    [TestMethod]
+    public void GeneratesClientCorrectly()
+    {
+        var calculator = new Calculator();
+        
+        calculator.Add(1, 1).Should().Be(2);
+        calculator.Multiply(1, 1).Should().Be(1);
+    }
+}
